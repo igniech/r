352db1e37bb955c3424a6045aaa2a7db708631ff
U-L-Komputer\Iwona <Iwona@L-Komputer.(none)>
2011-05-20 20:40:52 +0200

    buttony

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,16 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 #
-Kont.create(:imie => 'Katarzyna', :nazwisko => 'Tusk', :stanowisko => 'Specjalista ds rozwoju', :telefon1 => '586235854', :telefon2 => '639456987', :mail => 'katarzyna.tusk@bla.pl', :miasto => 'Gdynia')
+Kont.create(:imie => 'Katarzyna', :nazwisko => 'Tusk', :stanowisko => 'Specjalista ds. rozwoju', :telefon1 => '586235854', :telefon2 => '639456987', :mail => 'katarzyna.tusk@bla.pl', :miasto => 'Gdynia')
 
 Kont.create(:imie => 'Tadeusz', :nazwisko => 'Bobiski', :stanowisko => 'Dyrektor Sprzedaży', :telefon1 => '226263854', :telefon2 => '639453987', :mail => 'tadeusz.bobiski@bla.pl', :miasto => 'Warszawa')
+
+Kont.create(:imie => 'Stanisław', :nazwisko => 'Talaśka', :stanowisko => 'Specjalista ds. prawnych', :telefon1 => '246255854', :telefon2 => '852369741', :mail => 'stanislaw.talaska@bla.pl', :miasto => 'Katowice')
+
+Kont.create(:imie => 'Renata', :nazwisko => 'Mauser', :stanowisko => 'Dyrektor ds. Marketingu', :telefon1 => '586263854', :telefon2 => '603523654', :mail => 'renata.mauser@bla.pl', :miasto => 'Sopot')
+
+Kont.create(:imie => 'Wiesława', :nazwisko => 'Kwiatkowska', :stanowisko => 'Specjalista ds. IT', :telefon1 => '123333854', :telefon2 => '666666666', :mail => 'wieslawa.kwiatkowska@bla.pl', :miasto => 'Kraków')
+
+Kont.create(:imie => 'Tadeusz', :nazwisko => 'Zajewski', :stanowisko => 'Konserwator', :telefon1 => '546263854', :telefon2 => '603276198', :mail => 'tadeusz.Zajewski@bla.pl', :miasto => 'Olsztyn')
+
+Kont.create(:imie => 'Sebastian', :nazwisko => 'Aaron', :stanowisko => 'Kierownik sklepu', :telefon1 => '547263854', :telefon2 => '603676198', :mail => 'sebastian.aaron@bla.pl', :miasto => 'Olsztyn')
